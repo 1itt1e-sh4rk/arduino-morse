5376713be8dd862c63bf84f663b13c5a2781495e
1itt1e-sh4rk <754848662@qq.com>
2019-07-06 01:34:48 +0800

    提交总结

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# arduino-morse
+# arduino-morse(Arduino学习)
 
 ### 第一次作业 morse
+#### 提交时间: 7月3日晚上
 从Arduino串口读取一个字符串，并将其转换为莫尔斯码，以便从数据接口输出。使用前请将Morse文件夹复制到Arduino的libraries目录中。
+#### 文件结构
++ morse.ino 主程序
++ Morse
+    + keywords.txt 关键字说明
+    + Morse.cpp Morse电码输出类
+    + Morse.h Morse电码输出类的头文件
 
-### 第二次作业 one-nixie-tube
-七段显像管和CD4511的使用
+### 课堂作业一 car 
+#### 提交时间: 7月4日早上
+在老师给的代码基础上加上转向灯并在Tinkercad进行模拟仿真。
+#### 文件结构
++ car.ino 主程序
++ screenshot.png 电路连接图
+
+### 第二次作业 one-nixie-tube 
+#### 提交时间: 7月4日晚上
+七段显像管和CD4511模块的使用。
+#### 文件结构
++ one-nixie-tube.ino 主程序
++ screenshot.png 电路连接图
+
+### 课堂作业二 morse-onefile 
+#### 提交时间: 7月5日早上
+将morse项目的全部代码成一个文件并在Tinkercad进行模拟仿真。
+#### 文件结构
++ morse-onefile.ino 主程序
++ screenshot.png 电路连接图
+
+### 第三次作业 summary 
+#### 提交时间: 7月5日晚上
+课程学习总结。
+#### 文件结构
++ summary.md Markdown格式的总结报告
--- one-nixie-tube/one-nixie-tube.ino
+++ one-nixie-tube/one-nixie-tube.ino
@@ -13,11 +13,15 @@ int numbers[10][4] = {
 
 void setup() {
     // put your setup code here, to run once:
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
     Serial.begin(9600);
+  	digitalWrite(0, 0);
+    digitalWrite(1, 0);
+    digitalWrite(2, 0);
+    digitalWrite(3, 0);
 }
 
 void loop() {
@@ -26,11 +30,11 @@ void loop() {
     if(Serial.available() > 0)
     {
         num = Serial.read() - '0';
-        digitalWrite(0, numbers[num][3]);
-        digitalWrite(1, numbers[num][2]);
-        digitalWrite(2, numbers[num][1]);
-        digitalWrite(3, numbers[num][0]);
-        delay(100);
+        digitalWrite(2, numbers[num][3]);
+        digitalWrite(3, numbers[num][2]);
+        digitalWrite(4, numbers[num][1]);
+        digitalWrite(5, numbers[num][0]);
+        delay(10);
     }
     
 }
